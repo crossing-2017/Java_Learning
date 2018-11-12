e04008c56f03b92db1dab09fbbd7f9204ea7b736
crossing-2017 <crossing_2017@163.com>
2018-11-12 17:42:47 +0800

    测试

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,5 @@
 public class Test {
     public static void main(String[] args) {
         System.out.printf("HelloWorld!");
-        System.out.printf("HelloWorld!");
-        System.out.printf("HelloWorld!");
-        System.out.printf("HelloWorld!");
     }
 }
