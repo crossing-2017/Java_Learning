04e5bb6caf649f6d5b2b6384224362da0842c4bd
crossing-2017 <crossing_2017@163.com>
2018-11-12 17:34:05 +0800

    delete .ignore

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.printf("HelloWorld!");
+        System.out.printf("HelloWorld!");
+        System.out.printf("HelloWorld!");
+        System.out.printf("HelloWorld!");
     }
 }
