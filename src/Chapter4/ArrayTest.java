8e3a8105b4cfa095b992ff662654e09cbfa05b1f
crossing-2017 <crossing_2017@163.com>
2019-02-22 23:48:21 +0800

    第四章学习完成，新添下五子棋游戏

--- src/Chapter4/ArrayTest.java
+++ src/Chapter4/ArrayTest.java
@@ -10,6 +10,12 @@ public class ArrayTest {
             System.out.println(a[i]);
         }
 
+        //foreach循环的表示方法
+        for (int array :
+                a) {
+            System.out.println(array);
+        }
+
 
         //动态数组
 
@@ -17,5 +23,42 @@ public class ArrayTest {
         for(int i=0 ; i< c.length ; i++) {
             System.out.println(c[i]);
         }
+
+        Person[] p = new Person[2];
+        Person s1 = new Person();
+        s1.age = 12;
+        s1.height = 180.2;
+        Person s2 = new Person();
+        s2.height =120;
+        s2.age = 23;
+        p[0] = s1;
+        p[1] = s2;
+        p[0].Info();
+        p[1].Info();
+
+
+        //二维数组
+        int[][] d = new int[4][];
+        for (int[] indexs :
+                d) {
+            indexs = new int[2];
+            for (int index:
+                 indexs) {
+                index = 3;
+                System.out.println(index);
+            }
+        }
+
+        String[][] s = new String[][]{new String[3], {"Hello"}};
+
+
+    }
+}
+
+class Person{
+    public int age;
+    public double height;
+    public void Info(){
+        System.out.println("年龄是：" + age + ", 身高是：" + height);
     }
 }
---
+++ src/Chapter4/Gobang.java
@@ -0,0 +1,49 @@
+package Chapter4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Gobang {
+    private static int BOARD_SIZE = 10;
+    private String[][] board;
+
+    //初始化棋盘
+    public void initboard(){
+        board = new String[BOARD_SIZE][BOARD_SIZE];
+        for (int i=0 ; i<BOARD_SIZE ; i++){
+            for(int j=0 ; j<BOARD_SIZE ; j++){
+                board[i][j] = "+ ";
+            }
+        }
+    }
+
+    //打印棋盘
+    public void printBoard(){
+        for (int i=0 ; i<BOARD_SIZE ; i++){
+            for(int j=0 ; j<BOARD_SIZE ; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println("\n");
+        }
+    }
+
+
+    //主函数
+    public static void main (String args[])throws Exception{
+        Gobang gobang = new Gobang();
+        gobang.initboard();
+        gobang.printBoard();
+        System.out.println("请输入你的下棋的坐标， 以x,y的格式：");
+        //用于键盘输入
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while ((input = br.readLine()) != null){
+            String[] posStrArr = input.split(",");
+            int xPos = Integer.parseInt(posStrArr[0]);
+            int yPos = Integer.parseInt(posStrArr[1]);
+            gobang.board[yPos - 1][xPos - 1] = "O ";
+            gobang.printBoard();
+            System.out.println("请输入你的下棋的坐标， 以x,y的格式：");
+        }
+    }
+}
