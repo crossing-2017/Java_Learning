483334365860ca78e8b333ad675e9cb8b7e0bec5
crossing-2017 <crossing_2017@163.com>
2019-02-24 15:38:30 +0800

    上传昨天的更细

---
+++ src/Chapter5/ConstractorTest.java
@@ -0,0 +1,14 @@
+package Chapter5;
+
+public class ConstractorTest {
+    int foo;
+
+    public ConstractorTest(){
+        int foo = 0;
+        this.foo = 6;
+    }
+
+    public static void main(String[] args){
+        System.out.println(new ConstractorTest().foo);
+    }
+}
---
+++ src/Chapter5/OopTest1.java
@@ -0,0 +1,35 @@
+package Chapter5;
+
+/**
+ * @author Crossing
+ * @date 2019/02/23
+ */
+public class OopTest1 {
+    public static void main(String[] args){
+
+        Dog dog = new Dog();
+        dog.run();
+    }
+}
+
+
+class Dog{
+    private void jump(){
+        System.out.println("正在执行jump方法！");
+    }
+
+    /**
+     * 当新建一个对象后，此时，这里的this即之该对象
+     */
+    public void run(){
+        this.jump();
+        System.out.println("正在执行run方法！");
+    }
+
+    //也可以这样，在一个类中一个方法调用另一个方法时，this可以省略
+    /**
+    public void run(){
+        jump();
+        System.out.println("正在执行run方法！");
+    }*/
+}
